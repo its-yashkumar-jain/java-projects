359967529cbb838282973ba8a8cb303ea3652f33
jhamadhav <jhamadhav28@gmail.com>
2021-07-13 11:24:40 +0530

    salary added in employee program

--- lab-2/src/Employee.java
+++ lab-2/src/Employee.java
@@ -1,10 +1,11 @@
 public class Employee {
     String name, address;
-    int yrs;
+    int yrs, salary;
 
-    Employee(String name, int yrs, String address) {
+    Employee(String name, int yrs, String address, int salary) {
         this.name = name;
         this.yrs = yrs;
+        this.salary = salary;
         this.address = address;
     }
 
@@ -14,11 +15,11 @@ public class Employee {
 
     public static void main(String[] args) {
         System.out.println("\nName\t\tYear of joining\t\tAddress");
-        Employee e1 = new Employee("Robert", 1994, "64C-WallStreet");
+        Employee e1 = new Employee("Robert", 1994, "64C-WallStreet", 34567);
         e1.printEmployeeData();
-        Employee e2 = new Employee("Sam", 2000, "68D-WallStreet");
+        Employee e2 = new Employee("Sam", 2000, "68D-WallStreet", 345678);
         e2.printEmployeeData();
-        Employee e3 = new Employee("John", 1999, "26B-WallStreet");
+        Employee e3 = new Employee("John", 1999, "26B-WallStreet", 9876);
         e3.printEmployeeData();
 
         System.out.println();
