52c5e46291925751bb17817944fa0a1b7e00f962
jhamadhav <jhamadhav28@gmail.com>
2021-08-10 09:47:43 +0530

    lab4 init

---
+++ lab-4/src/GraphicsShape.java
@@ -0,0 +1,33 @@
+abstract class Figure {
+
+}
+
+class Open extends Figure {
+
+}
+
+class Line extends Open {
+
+}
+
+class PolyLine extends Open {
+
+}
+
+class Close extends Figure {
+
+}
+
+class Polygon extends Close {
+
+}
+
+class Eclipse extends Close {
+
+}
+
+public class GraphicsShape {
+    public static void main(String[] args) {
+
+    }
+}
